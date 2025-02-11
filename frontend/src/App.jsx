34acb5259fd5df78c0d6d056ba35cfaced03b066
Jun Lee <junlee@Mac.ssid1>
2025-02-10 19:51:35 -0500

    socket messages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import io from "socket.io-client";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // I want to connect to the socket.io server
+
+  // this sends a connection request to your socket io server
+  // but your backend is not listening to any connection requests yet
+  const socket = io("http://localhost:3000");
+
+  const [message, setMessage] = useState("");
+
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket.on("allMessages", (data) => {
+      setMessages(data);
+    });
+  }, []);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    // emit an event to the socket server
+    // emit takes 2 args:
+    // 1. the event name
+    // 2. the data you want to pass through
+    socket.emit("sendMessage", message);
+    setMessage("");
+  };
+
+  // terms for socket:
+  // 1. emit - send message
+  // 2. ON - listening to message
+  // you can use these anywhere
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ul>
+        {messages.map((message, index) => (
+          <li key={index}>{message}</li>
+        ))}
+      </ul>
+      <form onSubmit={sendMessage}>
+        <input
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+        />
+        <button>Send Message</button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
